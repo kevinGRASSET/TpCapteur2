8a4c2b8b502c0e02a881c9b6aaab1426e321b423
Kevin Grasset <grassetk@iut1i-605-a15.local>
2018-05-23 17:42:24 +0200

    Exercice 2

--- TPCapteurs/app/src/main/java/com/example/grassetk/tpcapteurs/TP2_Exercice1.java
+++ TPCapteurs/app/src/main/java/com/example/grassetk/tpcapteurs/TP2_Exercice1.java
@@ -12,6 +12,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
@@ -26,6 +27,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.List;
 
 public class TP2_Exercice1 extends FragmentActivity implements OnMapReadyCallback {
@@ -34,6 +36,7 @@ public class TP2_Exercice1 extends FragmentActivity implements OnMapReadyCallbac
     LocationManager locationManager;
     TextView Latitude2;
     TextView Longitude2;
+    TextView Distance;
 
 
     @Override
@@ -47,7 +50,7 @@ public class TP2_Exercice1 extends FragmentActivity implements OnMapReadyCallbac
 
         Latitude2 = (TextView) findViewById(R.id.textView);
         Longitude2 = (TextView) findViewById(R.id.textView2);
-
+        Distance = (TextView) findViewById(R.id.textView3);
 
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -68,7 +71,7 @@ public class TP2_Exercice1 extends FragmentActivity implements OnMapReadyCallbac
                 return;
             }
             if(locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 0, new LocationListener() {
                     @Override
                     public void onLocationChanged(Location location) {
 
@@ -79,9 +82,18 @@ public class TP2_Exercice1 extends FragmentActivity implements OnMapReadyCallbac
                         double longitude = location.getLongitude();
                         //Instancie la classe LatLng
                         LatLng latLng = new LatLng(latitude, longitude);
+
+                        LatLng latLng2 = new LatLng(location.getLatitude(), location.getLongitude());
+                        LatLng latLng3 = null;
                         //Instancie la classe Geocoder
                         Geocoder geocoder = new Geocoder(getApplicationContext());
+
+                        if(latLng3 == null)
+                        {
+                            latLng3 = new LatLng(location.getLatitude(), location.getLongitude());
+                        }
                         try {
+
                             geocoder.getFromLocation(latitude, longitude, 1);
                             List<Address> addressList = geocoder.getFromLocation(latitude, longitude, 1);
                             //Recupere le nom du pays
@@ -94,6 +106,9 @@ public class TP2_Exercice1 extends FragmentActivity implements OnMapReadyCallbac
                             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10.2f));
                             Latitude2.setText(String.valueOf(location.getLatitude()));
                             Longitude2.setText(String.valueOf(location.getLongitude()));
+
+                           CalculationByDistance(latLng3, latLng2);
+
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
@@ -121,10 +136,6 @@ public class TP2_Exercice1 extends FragmentActivity implements OnMapReadyCallbac
                 locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
                     @Override
                     public void onLocationChanged(Location location) {
-
-
-
-
                         //Recupere la latitude
                         double latitude = location.getLatitude();
 
@@ -170,17 +181,31 @@ public class TP2_Exercice1 extends FragmentActivity implements OnMapReadyCallbac
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+    }
+
+    public double CalculationByDistance(LatLng StartP, LatLng EndP) {
+        int Radius = 6371;// radius of earth in Km
+        double lat1 = StartP.latitude;
+        double lat2 = EndP.latitude;
+        double lon1 = StartP.longitude;
+        double lon2 = EndP.longitude;
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+                + Math.cos(Math.toRadians(lat1))
+                * Math.cos(Math.toRadians(lat2)) * Math.sin(dLon / 2)
+                * Math.sin(dLon / 2);
+        double c = 2 * Math.asin(Math.sqrt(a));
+        double valueResult = Radius * c;
+        double km = valueResult / 1;
+        DecimalFormat newFormat = new DecimalFormat("####");
+        int kmInDec = Integer.valueOf(newFormat.format(km));
+        double meter = valueResult % 1000;
+        int meterInDec = Integer.valueOf(newFormat.format(meter));
+        Distance.setText("" + meter + " M");
+        return Radius * c;
     }
 }
